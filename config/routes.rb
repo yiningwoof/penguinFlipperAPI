150d1bf5e4e9269d8aa3d13da6063d533b9e0210
Yining <yining.wang@utexas.com>
2020-01-26 20:02:06 -0600

    changed gem pg version and reinstalled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "api/v1/penguins#index"
 
   namespace :api do
     namespace :v1 do
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_12_20_160829) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_12_20_160829) do
   end
 
   create_table "cards", force: :cascade do |t|
-    t.integer "game_id"
-    t.integer "penguin_id"
+    t.bigint "game_id"
+    t.bigint "penguin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_cards_on_game_id"
@@ -57,4 +60,7 @@ ActiveRecord::Schema.define(version: 2019_12_20_160829) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cards", "games"
+  add_foreign_key "cards", "penguins"
 end
