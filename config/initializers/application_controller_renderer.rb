92350bf39f2a89db3c180da63e456b42bfaee7d2
Yining Wang <yining.wang@utexas.edu>
2020-01-27 15:44:32 -0600

    deleted initializer

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,7 +7,7 @@
 #   )
 # end
 
-ActiveRecord::Base.establish_connection!(
- :private_key_id   => ENV['private_key_id'],
- :private_key => ENV['private_key']
-)
+# ActiveRecord::Base.establish_connection!(
+#  :private_key_id   => ENV['private_key_id'],
+#  :private_key => ENV['private_key']
+# )
